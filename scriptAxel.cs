d4c1171d18763121f5209c9a0c35dd37b95521f5
AxelleAlbert <144026820+AxelleAlbert@users.noreply.github.com>
2023-12-06 10:02:43 +0100

    Create scriptAxel.cs

---
+++ scriptAxel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class test : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SettingsButton()
+    {
+        Debug.Log("Open Settings");
+    }
+
+    public void UserButton()
+    {
+        Debug.Log("Open User Settings");
+    }
+
+    public void NotifButton()
+    {
+        Debug.Log("Open Notifications Settings");
+    }
+}
